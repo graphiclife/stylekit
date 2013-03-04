0e7f28433bebd63796d08fbcec74730eec486312
Måns Severin <mans@graphiclife.com>
2013-03-04 09:55:17 +0100

    added license to StyleKit.h

--- StyleKit.h
+++ StyleKit.h
@@ -1,17 +1,23 @@
-//
-// StyleKit
-//
-// Copyright Graphiclife 2013. All Rights Reserved.
-//
-// The copyright to the computer program(s) herein
-// is the property of Graphiclife, Sweden. The
-// program(s) may be used and/or copied only with the
-// written permission of Graphiclife or in accordance
-// with the terms and conditions stipulated in the
-// agreement/contract under which the program(s) have
-// been supplied. This copyright notice must not be
-// removed.
-//
+// Copyright (C) 2013 Graphiclife
+
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #ifndef STYLE_KIT_H
 #define STYLE_KIT_H
